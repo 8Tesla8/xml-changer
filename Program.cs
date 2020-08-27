994a71e0edfae5fa005cff7af11caddf0a4d20cb
tesla <kela256@gmail.com>
2020-08-27 21:05:46 +0300

    add project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace SettingsChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentFolder = Directory.GetCurrentDirectory();                         //give path with \bin\debug\netcoreapp
+            string mainFolder = Path.GetFullPath(Path.Combine(currentFolder, @"..\..\..\"));  //go up for 3 folders
+
+
+            var configPath = mainFolder + "App.config";
+
+            var changer = new XMLChanger();
+            changer.Change(configPath);
+        }
+    }
+}
---
+++ XMLChanger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//for XML
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SettingsChanger
+{
+    public class XMLChanger
+    {
+        public void Change(string path)
+        {
+            XDocument config = XDocument.Load(path);
+
+            ChangeDBConfigs(config);
+            ChangeMySectionConfigs(config);
+
+            config.Save(path);
+        }
+
+        private void ChangeDBConfigs(XDocument config)
+        {
+            var nodes = config
+                       .Descendants("connectionStrings")
+                       .Elements("add").ToList();
+
+            foreach (var n in nodes)
+            {
+                if (n.Attribute("name").Value == "Connection1")
+                {
+                    n.Attribute("connectionString").Value = "Data Source=localhost;Initial Catalog=NewDB;";
+                }
+            }
+        }
+
+        private void ChangeMySectionConfigs(XDocument config)
+        {
+            var addNodes = config
+                       .Descendants("MySection")
+                       .Elements("SubSection")
+                       .Elements("add").ToList();
+
+            foreach (var addNote in addNodes)
+            {
+                var myItemNote = addNote.Elements("MyItem").First();
+
+                if (addNote.Attribute("Name").Value == "item1")
+                {
+                    myItemNote.Attribute("Param1").Value = "false";
+                    myItemNote.Attribute("Param2").Value = "2";
+                    myItemNote.Attribute("Param2").Value = "44";
+                }
+                else 
+                {
+                    myItemNote.Attribute("Param2").Value = "55";
+                }
+            }
+        }
+
+    }
+}
